62a28b6f56472d482d66edc62f86e041cc3fcd7b
Muaaz <muaazzahida@gmail.com>
2024-03-12 16:10:38 +0530

    Updated till creation of User in /api/auth in MongoDB

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,15 @@
 import connectToMongo from "./db.js";
 import express from "express";
 import authRoutes from './routes/auth.js';
-// import notesRoutes from './routes/notes.js';
+import notesRoutes from './routes/notes.js';
 connectToMongo();
 const app = express()
 const port = 3000
 
+app.use(express.json())
 //Available Routes
-
 app.use('/api/auth', authRoutes)
-// app.use('/api/notes', notesRoutes)
+app.use('/api/notes', notesRoutes)
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`)
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,13 +1,13 @@
 import express from "express";
 const router  = express.Router()
+import User from "../models/User.js"
 
-router.get('/', (req, res)=>{
-    const obj = {
-        name : 'hi',
-        email : 'hello@gmail.com'
-    }
-
-    res.json(obj)
+//create a user using "POST" request on  "/API/AUTH" . 
+router.post('/', (req, res)=>{
+    console.log(req.body)
+    const user = User(req.body)
+    user.save()
+    res.send(req.body)
 })
 
 export default router;
