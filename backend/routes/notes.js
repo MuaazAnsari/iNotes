bd981bd3f4ac92faeca46ec7f7f25b751e2d5e54
Muaaz <muaazzahida@gmail.com>
2024-08-27 23:23:44 +0530

    Update notes endpoint added

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -42,5 +42,42 @@ router.post('/addnotes',[
     } 
 });
 
+// ROUTE 3 : Update an existing notes using  PUT : /api/notes/updatenotes  "login required"
+router.put('/updatenotes/:id', fetchUser, async (req,res,next) =>{
+
+    try {
+        const {title, description, tag} = req.body;
+
+        const newNote = {};
+        // if title or description or tag exists, update that as newNote object
+        if(title) {newNote.title = title};
+        if(description) {newNote.description = description};
+        if(tag) {newNote.tag = tag};
+
+        // Find the note to be updated and update it
+        let note = await Notes.findById(req.params.id);
+
+        //if note doesnt exists
+        if(!note){
+            return res.status(404).send("Oops , Notes Not Found!");
+        }
+
+        // verify wether its the same user
+        if(note.user.toString() !== req.user.id){
+            return res.status(401).send("Unauthorised Access");
+        }
+
+        // update the note
+
+        note = await Notes.findByIdAndUpdate(req.params.id, {$set : newNote}, {new:true});
+
+        res.json(note);
+
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send("Internal Server error!");
+    } 
+});
+
 
 module.exports = router;
