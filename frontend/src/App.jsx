4fa255912267eb0187f101c7bace56ba8728494a
Muaaz <muaazzahida@gmail.com>
2024-08-30 11:43:19 +0530

    created , configured context api and setup form for adding notes in frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,24 @@
-import './App.css'
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
+import NoteState from "./context/Notes/noteState";
 
 function App() {
   return (
-    <>
-      <h1>This is iNotes</h1>
-    </>
-  )
+    <NoteState>
+      <Router>
+        <Navbar />
+        <div className="container">
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/about" element={<About />} />
+          </Routes>
+        </div>
+      </Router>
+    </NoteState>
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/components/About.jsx
@@ -0,0 +1,14 @@
+// import { useContext , useEffect} from "react";
+// import noteContext from "../context/Notes/noteContext";
+
+
+const About = () => {
+  
+  return (
+    <div>
+      This is About Page
+    </div>
+  )
+}
+
+export default About
